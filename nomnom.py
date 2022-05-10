b0cb7af1175880bfbb1b86f772867a60728c87df
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2022-05-10 08:25:57 -0600

    Update nomnom.py

--- nomnom.py
+++ nomnom.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import math
 pygame.init()  
-pygame.display.set_caption("ell")  # sets the window title
+pygame.display.set_caption("platformer")  # sets the window title
 screen = pygame.display.set_mode((1000, 1000))  # creates game screen
 screen.fill((0,0,0))
 clock = pygame.time.Clock() #set up clock
@@ -11,9 +11,10 @@ red = (255,0,0)
 
 
 Eel = pygame.image.load('eelface.png') #load your spritesheet
-Eel2 = pygame.image.load('eelface2.png')
-Eel.set_colorkey((255,255,255)) #this makes bright pink (255, 0, 255) transparent (sort of)
-Eel2.set_colorkey((255,255,255)) #this makes bright pink (255, 0, 255) transparent (sort of)
+Eel.set_colorkey((255,255,255))
+Eel2 = pygame.image.load('eelface2.png') #load your spritesheet
+Eel2.set_colorkey((255,255,255))
+
 fishy = pygame.image.load('fishy.png')
 fishy.set_colorkey((255,255,255))
 Back = pygame.image.load('background.png')
@@ -23,24 +24,26 @@ LEFT=0
 RIGHT=1
 UP = 2
 DOWN = 3
-Px= 600 #xpos of player
-Py= 700 #ypos of player
-Px2= 400
-Py2= 700 
-vx = 0 #x velocity of player
-vy = 0 #y velocity of player
-vx2 = 0
-vy2 = 0
+Px= 800 #xpos of player
+Py= 800 #ypos of player
+Px2 = 200
+Py2 = 200
 score = 0
 score2 = 0
 vx = 0 #x velocity of player
 vy = 0 #y velocity of player
+vx2 = 0
+vy2 = 0
 keys = [False, False, False, False] #this list holds whether each key has been pressed
 second = [False, False, False, False]
+
 frameWidth = 50
 frameHeight = 50
-RowNum = 0
+RowNum = 2
 frameNum = 0
+RowNum2 = 0
+frameNum2 = 0
+
 def CircleCollision(x1,x2,y1,y2, radius):
     if (math.sqrt((x2 - x1)**2 + (y2- y1)**2))<radius:
         return True
@@ -59,6 +62,8 @@ xpos=0
 ypos=0
 mousePos = (xpos, ypos)
 
+print('how fest u trynna go famalam 1-5')
+n = (int(input()))
 
 while not gameover: #GAME LOOP############################################################
     clock.tick(60) #FPS
@@ -113,52 +118,68 @@ while not gameover: #GAME LOOP##################################################
     
     #LEFT MOVEMENT
     if keys[LEFT]==True:
-        vx=-3
+        vx=-n
         vy=0
+        RowNum = 2
+        frameNum = 0
         direction = LEFT
         
     #Right Movement
     elif keys[RIGHT]==True:
-        vx=3
+        vx=n
         vy=0
+        RowNum = 0
+        frameNum = 0
         direction = RIGHT
     
       #JUMPING  
     if keys[UP]==True:
-        vy=-3
+        vy=-n
         vx=0
+        RowNum = 1
+        frameNum = 0
         direction = UP
         
       #DOWN
     if keys[DOWN]==True:
-        vy=+3
+        vy=+n
         vx=0
+        RowNum = 3
+        frameNum = 0
         direction = DOWN
     
     #LEFT MOVEMENT
     if second[LEFT]==True:
-        vx2=-3
+        vx2=-n
         vy2=0
+        RowNum2 = 2
+        frameNum2 = 0
         direction2 = LEFT
         
     #Right Movement
     elif second[RIGHT]==True:
-        vx2=3
+        vx2=n
         vy2=0
+        RowNum2 = 0
+        frameNum2 = 0
         direction2 = RIGHT
     
       #JUMPING  
     if second[UP]==True:
-        vy2=-3
+        vy2=-n
         vx2=0
+        RowNum2 = 1
+        frameNum2 = 0
         direction2 = UP
         
       #DOWN
     if second[DOWN]==True:
-        vy2=+3
+        vy2=+n
         vx2=0
+        RowNum2 = 3
+        frameNum2 = 0
         direction2 = DOWN
-        
+    #update player position
     Px+=vx 
     Py+=vy
     Px2+=vx2
@@ -167,18 +188,25 @@ while not gameover: #GAME LOOP##################################################
     #try to call the function here, use the new variables
     #(put the call inside an if statement, and only get new points for the circle when it's clicked on)
     if CircleCollision(num,Px, Py,num1, s)==True:
-        
         num = random.randrange(1, 800)
         num1 = random.randrange(1, 800)
         c1 = random.randrange(1, 255)
         c2 = random.randrange(1, 255)
         c3 = random.randrange(1, 255)
         s = random.randrange(10, 21)
-
         frameNum += 1
         score += 1
+        
+    elif CircleCollision(num,Px2, Py2,num1, s)==True:
+        num = random.randrange(1, 800)
+        num1 = random.randrange(1, 800)
+        c1 = random.randrange(1, 255)
+        c2 = random.randrange(1, 255)
+        c3 = random.randrange(1, 255)
+        s = random.randrange(10, 21)
+        frameNum2 += 1
         score2 += 1
-
+#player 1 warp zone
     if Px < 0:
         Px=999
     if Px > 999:
@@ -187,18 +215,27 @@ while not gameover: #GAME LOOP##################################################
         Py=999
     if Py > 999:
         Py=0
+#player 2 warp zone        
+    if Px2 < 0:
+        Px2=999
+    if Px2 > 999:
+        Px2=0
+    if Py2 < 0:
+        Py2=999
+    if Py2 > 999:
+        Py2=0   
     #Render Section ---------------------------
     screen.fill((0,0,255))
     screen.blit(Back, (0,0), (0,0,1000,1000))
     font = pygame.font.Font(None, 74)
     text = font.render(str(score),1, (0, 255, 0))
-    text2 = font.render(str(score),1, (0,255,0))
-    screen.blit(text2, (750, 10)) 
+    screen.blit(text, (750, 10))
+    text = font.render(str(score2),1, (0, 255, 0))
     screen.blit(text, (250, 10))
     screen.blit(fishy,(num, num1,25,25))
     screen.blit(Eel, (Px, Py), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
-    screen.blit(Eel2, (Px2, Py2), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))   
-  
+    screen.blit(Eel2, (Px2, Py2), (frameWidth*frameNum2, RowNum2*frameHeight, frameWidth, frameHeight))    
+
 
     pygame.display.flip()
 
